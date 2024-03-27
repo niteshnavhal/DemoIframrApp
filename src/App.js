64ceb27038e7e671792e573c22358a7b30908905
Nitesh <nitesh@ziwotech.com>
2024-03-27 11:22:20 +0530

    git New Changes

--- src/App.js
+++ src/App.js
@@ -2,22 +2,41 @@ import React, { useState, useEffect } from "react";
 
 function App() {
   const [url, setUrl] = useState("");
+  const [iframeWidth, setIframeWidth] = useState(100);
 
   const handleInputChange = (event) => {
     setUrl(event.target.value);
   };
 
+  const handleIframewidthChange = (event) => {
+    setIframeWidth(event.target.value);
+  };
+
   const handleLoadUrl = () => {
-    // You might want to do some validation on the URL here
     const iframe = document.getElementById("iframe");
     iframe.src = url;
   };
 
   useEffect(() => {
-    // Ensure DOM is fully loaded before accessing elements
     const iframe = document.getElementById("iframe");
     iframe.src = url;
-  }, [url]); // Empty dependency array ensures this effect runs only once after initial render
+  }, [url]);
+
+  useEffect(() => {
+    const iframe = document.getElementById("iframe");
+    const toStop = ["click", "mousemove", "keypress"];
+
+    toStop.forEach((eventType) => {
+      iframe.contentWindow.addEventListener(eventType, (event) => {
+        event.stopPropagation();
+      });
+    });
+  }, []);
+
+  useEffect(() => {
+    const iframe = document.getElementById("iframe");
+    iframe.style.width = `${iframeWidth}%`;
+  }, [iframeWidth]);
 
   return (
     <div>
@@ -29,10 +48,17 @@ function App() {
       />
       <button onClick={handleLoadUrl}>Load URL</button>
       <br />
+      <input
+        type="number"
+        value={iframeWidth}
+        onChange={handleIframewidthChange}
+        placeholder="Enter Iframe Number"
+      />
+      <br />
       <iframe
         id="iframe"
         title="Loaded URL"
-        style={{ width: "100%", height: "500px" }}
+        style={{ width: `${iframeWidth}%`, height: "500px" }}
       />
     </div>
   );
